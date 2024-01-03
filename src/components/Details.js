811d26773e95e579c6dfe917c9b90e2b088ce733
YashSharma412 <sharmayashsatiish@gmail.com>
2024-01-03 14:42:04 +0530

    Revert "test4"
    This reverts commit db37ee2949852b3b260e8f77c02e7ef7b26a3a4b.

--- src/components/Details.js
+++ src/components/Details.js
@@ -1,5 +1,6 @@
 import React from "react";
 
+<<<<<<< HEAD
 
 const Details = ({userData, userId}) => {
   // if(!user){
@@ -14,6 +15,21 @@ const Details = ({userData, userId}) => {
       <p><b>Email: </b> {MyUser.email}</p>
       <p><b>Phone: </b> {MyUser.phone}</p>
       <p><b>Website: </b> {MyUser.website}</p>
+=======
+const Details = ({user}) => {
+  return (
+    <div>
+      {
+        user ? <div>
+          <h1>User Details</h1>
+            <p><b>Name: </b> {user.name}</p>
+            <p><b>Username: </b> {user.username}</p>
+            <p><b>Email: </b> {user.email}</p>
+            <p><b>Phone: </b> {user.phone}</p>
+            <p><b>Website: </b> {user.website}</p>
+        </div> : <div>Loading...</div>
+      }
+>>>>>>> parent of db37ee2 (test4)
     </div>
   );
 };
