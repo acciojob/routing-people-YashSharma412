db37ee2949852b3b260e8f77c02e7ef7b26a3a4b
YashSharma412 <sharmayashsatiish@gmail.com>
2024-01-03 14:10:51 +0530

    test4

--- src/components/Details.js
+++ src/components/Details.js
@@ -1,18 +1,19 @@
 import React from "react";
 
+
 const Details = ({user}) => {
+  if(!user){
+    return (<div>Loading...</div>);
+  }
+  
   return (
     <div>
-      {
-        user ? <div>
-          <h1>User Details</h1>
-            <p><b>Name: </b> {user.name}</p>
-            <p><b>Username: </b> {user.username}</p>
-            <p><b>Email: </b> {user.email}</p>
-            <p><b>Phone: </b> {user.phone}</p>
-            <p><b>Website: </b> {user.website}</p>
-        </div> : <div>Loading...</div>
-      }
+      <h1>User Details</h1>
+      <p><b>Name: </b> {user.name}</p>
+      <p><b>Username: </b> {user.username}</p>
+      <p><b>Email: </b> {user.email}</p>
+      <p><b>Phone: </b> {user.phone}</p>
+      <p><b>Website: </b> {user.website}</p>
     </div>
   );
 };
