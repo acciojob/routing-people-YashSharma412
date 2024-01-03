db2916503cd0241babf9f925ec4353f52db08022
YashSharma412 <sharmayashsatiish@gmail.com>
2024-01-03 14:08:02 +0530

    test3

--- src/components/Details.js
+++ src/components/Details.js
@@ -6,12 +6,12 @@ const Details = ({user}) => {
       {
         user ? <div>
           <h1>User Details</h1>
-      <p><b>Name: </b> {user.name}</p>
-      <p><b>Username: </b> {user.username}</p>
-      <p><b>Email: </b> {user.email}</p>
-      <p><b>Phone: </b> {user.phone}</p>
-      <p><b>Website: </b> {user.website}</p>
-        </div> : <h1>Loading...</h1>
+            <p><b>Name: </b> {user.name}</p>
+            <p><b>Username: </b> {user.username}</p>
+            <p><b>Email: </b> {user.email}</p>
+            <p><b>Phone: </b> {user.phone}</p>
+            <p><b>Website: </b> {user.website}</p>
+        </div> : <div>Loading...</div>
       }
     </div>
   );
