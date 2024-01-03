9a474aff3852e831c1c861b01c07bc68a018a3b8
YashSharma412 <sharmayashsatiish@gmail.com>
2024-01-03 14:04:07 +0530

    implemented loading

--- src/components/Details.js
+++ src/components/Details.js
@@ -3,12 +3,16 @@ import React from "react";
 const Details = ({user}) => {
   return (
     <div>
-      <h1>User Details</h1>
+      {
+        user ? <div>
+          <h1>User Details</h1>
       <p><b>Name: </b> {user.name}</p>
       <p><b>Username: </b> {user.username}</p>
       <p><b>Email: </b> {user.email}</p>
       <p><b>Phone: </b> {user.phone}</p>
       <p><b>Website: </b> {user.website}</p>
+        </div> : <h1>Loading...</h1>
+      }
     </div>
   );
 };
