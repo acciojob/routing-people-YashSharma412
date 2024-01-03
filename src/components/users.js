1122365a1753b3b334a0afcc983b5f3fbff78f0e
YashSharma412 <sharmayashsatiish@gmail.com>
2024-01-03 13:49:18 +0530

    test1

--- src/components/users.js
+++ src/components/users.js
@@ -2,7 +2,7 @@ const users = [{
     id: 1,
     name: "Leanne Graham",
     username: "Bret",
-    email: "Sincere@april.briz",
+    email: "Sincere@april.biz",
     phone: "1-770-736-8031 x56442",
     website: "hildegard.org",
 }, {
